54eba0109d113aa4f54a85d01e29a0b24776c1a8
wuqiang <happyboy_tu@163.com>
2019-10-14 17:25:54 +0800

    完成动画

--- countdown.js
+++ countdown.js
@@ -4,9 +4,11 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-var endTime = new Date(2019, 9, 11, 23, 45, 23);
+var endTime = new Date(2019, 9, 15, 23, 45, 23);
 var curShowTimeSeconds = 0;
-// const colors = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"]
+
+var balls = [];
+const colors = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"]
 
 window.onload = function () {
 
@@ -33,54 +35,121 @@ function getCurrentShowTimeSeconds() {
 
 function update() {
 	var nextShowTimeSeconds = getCurrentShowTimeSeconds();
-	// var nextHours = parseInt(nextShowTimeSeconds / 3600)
-	// var nextMinutes = parseInt((nextShowTimeSeconds - nextHours * 3600) / 60)
+	var nextHours = parseInt(nextShowTimeSeconds / 3600)
+	var nextMinutes = parseInt((nextShowTimeSeconds - nextHours * 3600) / 60)
 	var nextSeconds = nextShowTimeSeconds % 60
 
-	// var curHours = parseInt(curShowTimeSeconds / 3600)
-	// var curMinutes = parseInt((curShowTimeSeconds - curHours * 3600) / 60)
+	var curHours = parseInt(curShowTimeSeconds / 3600)
+	var curMinutes = parseInt((curShowTimeSeconds - curHours * 3600) / 60)
 	var curtSeconds = curShowTimeSeconds % 60
 
-	if(nextSeconds!=curtSeconds){
+	if (nextSeconds != curtSeconds) {
+		if (parseInt(nextHours / 10) != parseInt(curHours / 10)) {
+			addBalls(MARGIN_LEFT, MARGIN_TOP, parseInt(curHours / 10))
+		}
+		if (parseInt(nextHours % 10) != parseInt(curHours % 10)) {
+			addBalls(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(curHours % 10))
+		}
+
+		if (parseInt(nextMinutes / 10) != parseInt(curMinutes / 10)) {
+			addBalls(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes / 10))
+		}
+		if (parseInt(nextMinutes % 10) != parseInt(curMinutes % 10)) {
+			addBalls(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes % 10))
+		}
+
+		if (parseInt(nextSeconds / 10) != parseInt(curtSeconds / 10)) {
+			addBalls(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(curtSeconds / 10))
+		}
+		if (parseInt(nextSeconds % 10) != parseInt(curtSeconds % 10)) {
+			addBalls(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(curtSeconds % 10))
+		}
 		curShowTimeSeconds = nextShowTimeSeconds
 	}
+	updateBalls()
 }
 
 function updateBalls() {
+	for (let i = 0; i < balls.length; i++) {
+		balls[i].x += balls[i].vx
+		balls[i].y += balls[i].vy
+		balls[i].vy += balls[i].g
+
+		if (balls[i].y >= WINDOW_HEIGHT - RADIUS) {
+			balls[i].y = WINDOW_HEIGHT-RADIUS;
+			balls[i].vy = - balls[i].vy*0.75;
+		}
+	}
+
+	var cnt = 0
+	for (var i = 0; i < balls.length; i++)
+		if (balls[i].x + RADIUS > 0 && balls[i].x - RADIUS < WINDOW_WIDTH)
+			balls[cnt++] = balls[i]
+
+	while (balls.length > cnt) {
+		balls.pop();
+	}
+
 
 }
 
 function addBalls(x, y, num) {
 
+	for (var i = 0; i < digit[num].length; i++)
+		for (var j = 0; j < digit[num][i].length; j++)
+			if (digit[num][i][j] == 1) {
+				var aBall = {
+					x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+					y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+					g: 1.5 + Math.random(),
+					vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+					vy: -5,
+					color: colors[Math.floor(Math.random() * colors.length)]
+				}
+
+				balls.push(aBall)
+			}
 }
 
 function render(cxt) {
-	cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
 
-	var hours = parseInt(curShowTimeSeconds / 3600)
+	cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	var hours = parseInt(curShowTimeSeconds / 3600);
 	var minutes = parseInt((curShowTimeSeconds - hours * 3600) / 60)
 	var seconds = curShowTimeSeconds % 60
 
 	renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hours / 10), cxt)
 	renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hours % 10), cxt)
 	renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, cxt)
-	renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes / 10), cxt)
-	renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes % 10), cxt)
-	renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, cxt)
-	renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds / 10), cxt)
-	renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds % 10), cxt)
+	renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes / 10), cxt);
+	renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes % 10), cxt);
+	renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, cxt);
+	renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds / 10), cxt);
+	renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds % 10), cxt);
+
+	for (var i = 0; i < balls.length; i++) {
+		cxt.fillStyle = balls[i].color;
+
+		cxt.beginPath();
+		cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI, true);
+		cxt.closePath();
+
+		cxt.fill();
+	}
 }
 
 function renderDigit(x, y, num, cxt) {
-	cxt.fillStyle = 'rgb(0, 102, 153)'
-	for (let i = 0; i < digit[num].length; i++) {
-		for (let j = 0; j < digit[num][i].length; j++) {
-			if (digit[num][i][j] === 1) {
+
+	cxt.fillStyle = "rgb(0,102,153)";
+
+	for (var i = 0; i < digit[num].length; i++)
+		for (var j = 0; j < digit[num][i].length; j++)
+			if (digit[num][i][j] == 1) {
 				cxt.beginPath();
 				cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 * (RADIUS + 1) + (RADIUS + 1), RADIUS, 0, 2 * Math.PI)
 				cxt.closePath()
+
 				cxt.fill()
 			}
-		}
-	}
 }
